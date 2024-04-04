188ceae238ca7110fa7e59da7355a4092b6d88f0
IskraYuriyevna <1718318150@qq.com>
2024-04-05 02:34:58 +0800

    2024040502

--- homework2/homework2/N.c
+++ homework2/homework2/N.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
-    int n,strp;
-    scanf("%d", &n);
-    char num[20];
-    sprintf(num, "%d", n);
-
-    for (int i = 1; i <= 10000; i++) {
-        char current[20];
-        sprintf(current, "%d", i);
-        if (strstr(current, num) != NULL) {
-            strp= strlen(current) - strlen(num) + 1;
-        }
+    int Digital;
+    scanf("%d", &Digital);
+    char StrDigital[12]; 
+    sprintf(StrDigital, "%d", Digital);
+    int Quantity = 1024;
+    char* queue = (char*)malloc(Quantity * sizeof(char));
+    
+	if (queue == NULL) {
+        return 1; 
     }
+    queue[0] = '\0'; 
 
-    printf("%d",strp);
+    int num = 1;
+    int len = 0; 
+	
+    while (1) {
+        char Cache[20]; 
+        sprintf(Cache, "%d", num);
+        
+        int CacheLen = strlen(Cache);
+        while (len + CacheLen >= Quantity) { 
+            Quantity *= 2;
+            queue = (char*)realloc(queue, Quantity * sizeof(char));
+            if (queue == NULL) {
+                return 1; 
+            }
+        }
+        
+        strcat(queue, Cache); 
+        len += CacheLen;
+        
+        char* found = strstr(queue, StrDigital); 
+        if (found != NULL) {
+            printf("%ld\n", (found - queue) + 1); 
+            break;
+        }
+        
+        num++;
+    }
+    
+    free(queue);
     return 0;
 }
