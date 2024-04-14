71594f270ff7c3af381ceaa0a9b543c1035b1e52
IskraYuriyevna <1718318150@qq.com>
2024-04-15 05:19:25 +0800

    20240401142

--- homework2/4_array/E13.asm
+++ homework2/4_array/E13.asm
@@ -3,6 +3,64 @@
 section .bss
 N equ 10
 a resd N
+b resd N
 
 section .text
 global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    mov ecx,N
+    xor ebx,ebx
+    jmp .input
+    
+.input:
+    GET_DEC 4,[a+4*ebx]
+    inc ebx
+    loop .input
+    xor ebx,ebx
+    mov ecx,10
+    jmp .outloop
+    
+.outloop:
+    cmp ebx,N
+    je .endoutloop
+    mov eax,dword[a+4*ebx]
+    jmp .inloop
+    
+.inloop:
+    cdq
+    idiv ecx
+    cdq
+    idiv ecx
+    cmp edx,0
+    je .rt
+    jne .endinloop
+    
+.rt:
+    mov eax,dword[a+4*ebx]
+    mov dword[b+4*ebx],eax
+    jmp .endinloop
+
+.endinloop:
+    inc ebx
+    jmp .outloop
+    
+.endoutloop:
+    xor ebx,ebx
+    jmp .out
+    
+.out:
+    cmp ebx,N
+    je .end
+    cmp dword[b+4*ebx],0
+    je .skip
+    PRINT_DEC 4,[b+4*ebx]
+    PRINT_STRING " "
+    
+.skip
+    inc ebx
+    jmp .out
+    
+.end:
+    xor eax,eax
+    ret
