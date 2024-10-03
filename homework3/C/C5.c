d2d544784ba4048722ea83adae6eca6a16c3dd90
IskraYuriyevna <1718318150@qq.com>
2024-10-03 03:14:32 +0300

    modified:   homework3/C/C5.c 	new file:   homework3/C/output/C5.exe

--- homework3/C/C5.c
+++ homework3/C/C5.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <String.h>
 
-void convert_string(char str[],int len) {;
+//  void convert_string(char str[],int len) {
+//     for (int i = 0; i < len; i++) {
+//         if (str[i] >= 'a' && str[i] <= 'z') {
+//             continue;
+//         } else if (str[i] >= 'A' && str[i] <= 'Z') {
+//             char temp = str[i];
+//             for (int j = i; j <len; j++) {
+//                 if (str[j] >= 'a' && str[j] <= 'z'){
+//                     str[i] = str[j];
+//                     str[j] = temp;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
 
+
+char *strcat(char *dest, const char *src) {
+    char *ptr = dest; 
+
+    while (*ptr != '\0') {
+        ptr++;
+    }
+
+    while (*src != '\0') {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+    *ptr = '\0';
+
+    return dest;
+}
+
+void convert_string(char str[],int len) {
+    char low[len],upr[len];
+    int leb_l=0,leb_u=0;
     for (int i = 0; i < len; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
-            continue;
+            low[leb_l]=str[i];
+            leb_l++;
         } else if (str[i] >= 'A' && str[i] <= 'Z') {
-            char temp = str[i];
-            for (int j = i; j <len; j++) {
-                if (str[j] >= 'a' && str[j] <= 'z'){
-                    str[i] = str[j];
-                    str[j] = temp;
-                    break;
-                }
-            }
+            upr[leb_u]=str[i];
+            leb_u++;
         }
     }
+    low[leb_l] = '\0';
+    upr[leb_u] = '\0';
+    strcat(low,upr);
+    for (int i=0;i<len;i++){
+        str[i] = low[i];
+    }
 }
+
 
 int main(){
     char *arr = NULL;
