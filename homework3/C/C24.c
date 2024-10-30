da45a6ad16fbf3c627ba07199c96899f4b00968d
IskraYuriyevna <1718318150@qq.com>
2024-10-31 04:13:37 +0800

    修改：     homework3/C/C24.c 	新文件：   homework3/C/cnt.txt 	新文件：   homework3/C/cnta 	修改：     homework3/C/input.txt

--- homework3/C/C24.c
+++ homework3/C/C24.c
@@ -1,1 +1,57 @@
 #include<stdio.h>
+#include<string.h>
+
+#define SIZE 1001
+
+int main(int argc,char **argv){
+    FILE *fin,*fout;
+    char buf[SIZE];
+    int cnta=0,cntl=0,flga=0;
+
+    if(argc<3){
+        fprintf(stderr,"Something's wrong");
+        return 1;
+    }
+    fin = fopen(argv[1],"r");
+    if(fin == NULL){
+        fprintf(stderr,"Something's wrong");
+        return 2;
+    }
+    fout = fopen(argv[2],"w");
+    if(fout == NULL){
+        fprintf(stderr,"Something's wrong"); 
+        fclose(fin);
+        return 2;
+    }
+
+    while(fgets(buf,SIZE,fin) != NULL){
+        int len = strlen(buf);
+        for (int i=0;i<len;i++){
+            if((buf[i]==' ')||(buf[i]=='.')){
+                if(buf[i-1]=='a'){
+                    cnta++;
+                    flga++;
+                    // printf("find a at %d\n",i);
+                }
+            }
+            if((buf[i]=='\n')||(buf[i]==EOF)){
+                if (buf[i-1]=='a'){
+                    cnta++;
+                    flga++;
+                    // printf("find a on end at %d\n",i);
+                }
+                if(flga){
+                    cntl++;
+                    flga = 0;
+                    // printf("another line,i = %d\n",i);
+                }
+            }
+        }
+    }
+
+    fprintf(fout,"%d %d",cnta,cntl);
+
+    fclose(fin);
+    fclose(fout);
+    return 0;
+}
