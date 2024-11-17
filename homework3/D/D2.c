d18434f73c5cd6a410a583c1e574f250d9e478a2
IskraYuriyevna <1718318150@qq.com>
2024-11-17 21:10:26 +0800

    新文件：   D2.txt 	新文件：   homework3/D/D2 	修改：     homework3/D/D2.c 	新文件：   homework3/D/D7 	修改：     homework3/D/output/D2 	新文件：   homework3/D/test.c

--- homework3/D/D2.c
+++ homework3/D/D2.c
@@ -7,9 +7,10 @@
 #define BUF_SIZE 99
 
 int main(int argc,char **argv){
-    int fd1,fd2,size,cnt=0,i=0,j=0,k=0;
+    int fd1,fd2,size,cnt,sin=0,sout=0,i=0,j=0,k=0;
     char buf[BUF_SIZE];
     char *in = NULL;
+    char *out = NULL;
 
     if(argc<3){
         printf("Use: %s flie_source file_dest\n",argv[0]);
@@ -27,46 +28,51 @@ int main(int argc,char **argv){
     fd2 = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC,s.st_mode);
     if(fd2==-1){
         perror("open2");
-        close(fd2);
+        close(fd1);
         return 2;
     }
 
     int sizein = BUF_SIZE;
     in = (char *)malloc(sizein*sizeof(char));
     if(in == NULL){
-        printf("Can't allocate memory\n");
+        fprintf(stderr,"Can't allocate memory\n");
         return 1;
     }
 
     while((size = read(fd1,buf,BUF_SIZE)) > 0){
         for(i = 0;i<size;i++){
-            in[cnt]=buf[i];
-            cnt++;
+            in[sin]=buf[i];
+            sin++;
         }
-        if (cnt == sizein){
+        if (sin == sizein){
             char *buf2 = NULL;
             sizein *= 2;
             buf2 = (char*)realloc(in,sizein*sizeof(char));
             if(buf2==NULL){
                 sizein /= 2;
-                printf("Can't realloc memory\n");
+                fprintf(stderr,"Can't realloc memory\n");
                 break;
             }else{
                 in = buf2;
             }
         }
     }
-    //printf("cnt= %d,sizein = %d\n",cnt,sizein);
+    //printf("sin= %d,sizein = %d\n",sin,sizein);
 
-    sizein = cnt;
-    char out[sizein*2];
-    cnt = 0;j=0;
+    int sizeout = sin;
+    out = (char *)malloc(sizein*sizeof(char));
+    if(out == NULL){
+        fprintf(stderr,"Can't allocate memory\n");
+        return 1;
+    }
+    cnt = 0;j=0;sout=0;
     for(i=0;i<sizein;i++){
         if(((in[i]!=',')&(in[i]!='.')&(in[i]!=' ')&(in[i]!='\n')))
         {
             out[j]=in[i]; 
             cnt = cnt+1;
             j++;
+            sout++;
         }else{
             if((cnt!=0)&(cnt<=99)){
                 if((cnt>0)&(cnt<=9)){
@@ -81,11 +87,25 @@ int main(int argc,char **argv){
             out[j]=in[i];
             j++;
             cnt=0;
+            sout++;
+        }
+        if (sout == sizeout){
+            char *bufout = NULL;
+            sizeout *= 2;
+            bufout = (char*)realloc(out,sizeout*sizeof(char));
+            if(bufout==NULL){
+                sizeout /= 2;
+                fprintf(stderr,"Can't realloc memory\n");
+                break;
+            }else{
+                out = bufout;
+            }
         }
     }
     write(fd2,out,j-1);
 
     free(in);
+    free(out);
     close(fd1);
     close(fd2);    
     return 0;
