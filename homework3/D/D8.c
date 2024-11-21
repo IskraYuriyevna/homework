8e8d990cca68e5d117d9e2527b8d54a3c7fadbae
IskraYuriyevna <1718318150@qq.com>
2024-11-22 01:34:04 +0800

    修改：     homework3/D/D8.c 	新文件：   homework3/D/domus 	新文件：   homework3/D/family

--- homework3/D/D8.c
+++ homework3/D/D8.c
@@ -8,25 +8,21 @@ int main(){
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         pid = fork();
-        //创建子进程，并将其进程号写入pid
         if(pid==-1){perror("fork");return 1;}
-        //创建进程失败时的错误处理
+        num++;
+        //创建子进程，并将其进程号写入pid
         if(pid==0){
             //pid == 0也就是在子进程运行的时候
-            num++;
             printf("son %d\n",num);
             fflush(stdout);
             fprintf(stderr,"PID=%d PPID=%d\n",getpid(),getppid());
             fflush(stderr);
-        }else{
-            break;
-            //如果在父进程，停止循环。防止父进程反复输出。
+            break;//如果在子进程，停止循环。防止反复输出和产生下一次分支。
         }
         wait(NULL);
-        //父进程等待子进程结束后再结束
     }
-
-    wait(NULL);
+    //wait(NULL);
+    for(int i=0;i<n;i++){wait(NULL);}
     //等待所有子进程结束之后再结束
     return 0;
 }
