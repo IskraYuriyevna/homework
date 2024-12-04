1df4512e6cd0a291770740ec4924f6798b825a2c
IskraYuriyevna <1718318150@qq.com>
2024-12-05 02:04:41 +0800

    修改：     homework3/D/D18.c 	修改：     homework3/D/D19.c 	新文件：   homework3/D/pps

--- homework3/D/D18.c
+++ homework3/D/D18.c
@@ -1,47 +1,56 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<sys/ipc.h>
-#include<sys/sem.h>
-#include<sys/wait.h>
+#include<unistd.h>// Unix标准函数的头文件，比如pipe和fork
+#include<sys/ipc.h>// 进程间通讯的头文件，semget,semctl和semop
+#include<sys/sem.h>// 信号量操作的头文件,struct,sembuf和SETVAL等
+#include<sys/wait.h>// 等待进程状态改变的头文件，基本上就用到wait
 
 int main(){
     int cnt = 0,Cnt_Max;
     int fd[2];
+    // 定义两个信号量操作结构体P1和V1，分别用于等待（P操作）和释放（V操作）信号量0。
     struct sembuf P1={0,-1,0},V1={0,1,0};
+    // 定义另外两个信号量操作结构体P2和V2，分别用于等待（P操作）和释放（V操作）信号量1。
     struct sembuf P2={1,-1,0},V2={1,1,0};
+    // 使用ftok函数生成一个键值，用于创建信号量。"/bin/ls"是路径名，8是项目ID。
     key_t key = ftok("/bin/ls",8);
+    
     if(key<0){perror("ftok");return 1;}
 
+    //使用semget函数创建一个包含两个信号量的信号量集，IPC_CREAT标志表示如果信号量集不存在则创建它，0666是权限设置。
+    //如果 key 为非零值，系统会根据这个键值查找一个已经存在的信号量集。
+    //如果找到了，就返回这个信号量集的标识符；如果没有找到，就会根据后面的参数创建一个新的信号量集。
+    //在八进制权限中，0666 表示文件所有者、文件所在组以及其他用户都有读写权限（即 rw-rw-rw-）。
+    //这个权限设置允许多个用户对信号量进行操作。
     int semid = semget(key,2,IPC_CREAT | 0666);
     //创建两个信号量
     if(semid < 0){perror("semget");return 1;}
-    semctl(semid,0,SETVAL,0);// s1 = 0
-    semctl(semid,1,SETVAL,0);// s2 = 0
+    semctl(semid,0,SETVAL,0);// s1 = 0，使用semctl函数设置信号量0的值为0。
+    semctl(semid,1,SETVAL,0);// s2 = 0，使用semctl函数设置信号量1的值为0。
 
     if(pipe(fd)<0){perror("pipe");return 1;}
     scanf("%d",&Cnt_Max);
     pid_t pid = fork();
     if(pid<0){perror("fork");return 1;}
-    if(pid>0){
+    if(pid>0){//father
         write(fd[1],&cnt,sizeof(int));
-        semop(semid,&V1,1);
+        semop(semid,&V1,1);// 父进程释放信号量0。
         while(cnt<Cnt_Max){
-            semop(semid,&P2,1);
+            semop(semid,&P2,1);// 父进程等待信号量1。
             read(fd[0],&cnt,sizeof(int));
             printf("father %d\n",cnt++);fflush(stdout);
             write(fd[1],&cnt,sizeof(int));
-            semop(semid,&V1,1);
+            semop(semid,&V1,1);// 父进程释放信号量0。
         }
         close(fd[0]);close(fd[1]);
         wait(NULL);
-        semctl(semid,0,IPC_RMID,0);
-    }else{
+        semctl(semid,0,IPC_RMID,0);// 父进程删除信号量集。
+    }else{//son
         while(cnt < Cnt_Max){
-            semop(semid,&P1,1);
+            semop(semid,&P1,1);// 子进程等待信号量0。
             read(fd[0],&cnt,sizeof(int));
             printf("son %d\n",cnt++);fflush(stdout);
             write(fd[1],&cnt,sizeof(int));
-            semop(semid,&V2,1);
+            semop(semid,&V2,1);// 子进程释放信号量1。
         }
         close(fd[0]);close(fd[1]);
     }
--- homework3/D/D19.c
+++ homework3/D/D19.c
@@ -9,12 +9,91 @@ typedef struct{
     int Inc;
 }cnt;
 
+volatile sig_atomic_t flag = 1;
 int main(){
-    int cnt = 0;
+    cnt Cnt;
     int fd[2];
-    struct sembuf P1={},V1={};
-    struct sembuf P2={},V2={};
+    struct sembuf P1={0,-1,0},V1={0,1,0};
+    struct sembuf P2={1,-1,0},V2={1,1,0};
+    struct sembuf P3={2,-1,0},V3={2,1,0};
+    struct sembuf P4={3,-1,0},V4={3,1,0};
+    pid_t pid,pid2;
+
     key_t key = ftok("/bin/ls",8);
+    if(key<0){perror("ftok");return 0;}
 
+    int semid = semget(key,4,IPC_CREAT|0666);
+    if(semid<0){perror("semget");return 1;}
+    semctl(semid,0,SETVAL,0);//s1=0
+    semctl(semid,1,SETVAL,0);//s2=0
+    semctl(semid,2,SETVAL,0);//s3=0
+    semctl(semid,3,SETVAL,0);//s4=0
 
+    if(pipe(fd)<0){perror("pipe");return 1;}
+
+    scanf("%d %d",&Cnt.Dec,&Cnt.Inc);
+
+    pid = fork();
+    if(pid<0){perror("fork");return 1;}
+    if(pid>0){
+        pid2 = fork();
+        if(pid2<0){perror("fork");return 1;}
+        if(pid2>0){//father
+            write(fd[1],&Cnt,sizeof(cnt));
+            semop(semid,&V1,1);
+            while(Cnt.Inc<Cnt.Dec){
+                if(semop(semid,&P2,1)==0){
+                    read(fd[0],&Cnt,sizeof(cnt));
+                    if(Cnt.Inc<=Cnt.Dec){
+                        printf("father %d %d\n",Cnt.Dec,Cnt.Inc);fflush(stdout);
+                        Cnt.Dec--;Cnt.Inc++;
+                    }
+                    write(fd[1],&Cnt,sizeof(cnt));
+                    semop(semid,&V3,1);
+                }
+                if(semop(semid,&P4,1)==0){
+                    read(fd[0],&Cnt,sizeof(cnt));
+                    if(Cnt.Inc<=Cnt.Dec){
+                        printf("father %d %d\n",Cnt.Dec,Cnt.Inc);fflush(stdout);
+                        Cnt.Dec--;Cnt.Inc++;
+                    }
+                    write(fd[1],&Cnt,sizeof(cnt));
+                    semop(semid,&V1,1);
+                }
+
+            }
+            close(fd[0]);close(fd[1]);
+            wait(NULL);wait(NULL);
+            semctl(semid,0,IPC_RMID,0);
+        }else{//son2
+            while(Cnt.Inc<Cnt.Dec){
+                semop(semid,&P3,1);
+                read(fd[0],&Cnt,sizeof(cnt));
+                if(Cnt.Inc<=Cnt.Dec){
+                    printf("son2 %d %d\n",Cnt.Dec,Cnt.Inc);fflush(stdout);
+                    Cnt.Dec--;Cnt.Inc++;
+                }
+                write(fd[1],&Cnt,sizeof(cnt));
+                semop(semid,&V4,1);
+            }
+            close(fd[0]);close(fd[1]);
+        }
+    }else{//son1
+        if(flag){
+            if(Cnt.Inc==Cnt.Dec){printf("son1 %d %d\n",Cnt.Dec,Cnt.Inc);fflush(stdout);}
+            flag--;
+        }
+        while(Cnt.Inc<Cnt.Dec){
+            semop(semid,&P1,1);
+            read(fd[0],&Cnt,sizeof(cnt));
+            if(Cnt.Inc<=Cnt.Dec){
+                printf("son1 %d %d\n",Cnt.Dec,Cnt.Inc);fflush(stdout);
+                Cnt.Dec--;Cnt.Inc++;
+            }
+            write(fd[1],&Cnt,sizeof(cnt));
+            semop(semid,&V2,1);
+        }
+        close(fd[0]);close(fd[1]);
+    }
+    return 0;
 }
