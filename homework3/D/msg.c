2c460344f37c2b41cd65b340a06e859c1afdcf89
IskraYuriyevna <1718318150@qq.com>
2024-12-14 22:47:37 +0800

    要提交的变更： 	删除：     homework3/D/D13 	删除：     homework3/D/D14 	删除：     homework3/D/D15 	删除：     homework3/D/D15b 	删除：     homework3/D/D16 	删除：     homework3/D/D17 	删除：     homework3/D/D17o 	删除：     homework3/D/D2 	新文件：   homework3/D/D20.c 	新文件：   homework3/D/D21.c 	新文件：   homework3/D/D22 	新文件：   homework3/D/D22.c 	新文件：   homework3/D/D22r 	新文件：   homework3/D/D22r.c 	删除：     homework3/D/D5 	删除：     homework3/D/D7 	删除：     homework3/D/D8 	新文件：   homework3/D/aaa.txt 	删除：     homework3/D/domus 	删除：     homework3/D/family 	新文件：   homework3/D/msg.c 	重命名：   homework3/D/D14c -> homework3/D/msg2 	新文件：   homework3/D/msg2.c 	新文件：   homework3/D/msg3.c 	新文件：   homework3/D/msgpingpong.c 	新文件：   homework3/D/msgpingpong2.c 	删除：     homework3/D/out 	删除：     homework3/D/output.bin 	删除：     homework3/D/output.txt 	删除：     homework3/D/pps 	删除：     homework3/D/redir 	删除：     homework3/D/se 	删除：     homework3/D/sem 	修改：     "homework3/\346\223\215\344\275\234\347\263\273\347\273\237\351\242\230\345\272\223\345\206\214.md"

---
+++ homework3/D/msg.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<unistd.h>
+#include<string.h>
+
+struct msgbuf
+{
+    long mtype;
+    char data[256];
+};
+
+int main(){
+    struct msgbuf message;
+    char str[256];
+
+    key_t key = ftok("aaa.txt",8);
+    if(key<0){perror("ftok");return 1;}
+
+    int msgid = msgget(key,IPC_CREAT | 0666);
+    if(msgid<0){perror("msgget");return 1;}
+
+    while(1){
+        fgets(str,255,stdin);
+        strcpy(message.data,str);
+        switch(str[0]){
+            case'a':
+            case'A':
+                message.mtype = 1;
+                msgsnd(msgid,&message,strlen(str)+1,0);
+                break;
+            case 'b':
+            case 'B':
+                message.mtype = 2;
+                msgsnd(msgid,&message,strlen(str)+1,0);
+                break;
+            case 'q':
+            case 'Q':
+                strcpy(message.data,"Quit");
+                message.mtype = 1;
+                msgsnd(msgid,&message,strlen(("Quit")+1),0);
+                message.mtype = 2;
+                msgsnd(msgid,&message,strlen(("Quit")+1),0);
+                sleep(1);
+                msgctl(msgid,IPC_RMID,0);//delete
+                return 0;
+        } 
+    }
+}
---
+++ homework3/D/msg3.c
@@ -0,0 +1,31 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+struct msgbuf
+{
+    long mtype;
+    char data[256];
+};
+
+int main(){
+    struct msgbuf message;
+
+    key_t key=ftok("aaa.txt",8);
+    if(key<0){perror("ftok");return 1;}
+
+    int msgid = msgget(key,IPC_CREAT|0666);
+    if(msgid<0){perror("msgget");return 1;}
+
+    while(1){
+        msgrcv(msgid,&message,256,2,0);
+        if (strcmp(message.data,"Quit")==0){
+            break;
+        }else{
+            printf("Process3 received: %s\n",message.data);
+        }
+    }
+    return 0;
+}
