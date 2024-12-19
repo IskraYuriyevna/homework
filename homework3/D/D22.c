cda03a3a3f1367080c2959e23e7e153d8dafe5e3
IskraYuriyevna <1718318150@qq.com>
2024-12-20 04:04:44 +0800

    删除：     homework3/C/output/C0.exe 	删除：     homework3/C/output/C1.exe 	删除：     homework3/C/output/C10.exe 	删除：     homework3/C/output/C11.exe 	删除：     homework3/C/output/C12.exe 	删除：     homework3/C/output/C13.exe 	删除：     homework3/C/output/C14.exe 	删除：     homework3/C/output/C15.exe 	删除：     homework3/C/output/C16 	删除：     homework3/C/output/C17.exe 	删除：     homework3/C/output/C18.exe 	删除：     homework3/C/output/C19.exe 	删除：     homework3/C/output/C2 	删除：     homework3/C/output/C2.exe 	删除：     homework3/C/output/C20 	删除：     homework3/C/output/C20o 	删除：     homework3/C/output/C22 	删除：     homework3/C/output/C22r 	删除：     homework3/C/output/C26 	删除：     homework3/C/output/C3.exe 	删除：     homework3/C/output/C4.exe 	删除：     homework3/C/output/C4f.exe 	删除：     homework3/C/output/C5 	删除：     homework3/C/output/C5.exe 	删除：     homework3/C/output/C6.exe 	删除：     homework3/C/output/C7.exe 	删除：     homework3/C/output/C8.exe 	删除：     homework3/C/output/C9.exe 	删除：     homework3/C/output/C9r.exe 	删除：     homework3/C/output/fileex 	修改：     homework3/D/D22.c 	删除：     homework3/D/D22n.c 	修改：     homework3/D/D25.c 	修改：     homework3/D/a.out

--- homework3/D/D22.c
+++ homework3/D/D22.c
@@ -2,10 +2,10 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/ipc.h>
-#include<sys/wait.h>
+#include<sys/types.h>
 #include<sys/msg.h>
 #include<string.h>
-
+#include<sys/wait.h>
 
 struct msgbuf{
     long mtype;
@@ -17,7 +17,7 @@ int main(){
     struct msgbuf message;
     pid_t pid1,pid2,pid3;
 
-    key_t key = ftok("aaa.txt",8);
+    key_t key = ftok("/bin/ls",8);
     if(key<0){perror("ftok");return 1;}
     
     int msgid = msgget(key,IPC_CREAT | 0666);
@@ -25,9 +25,14 @@ int main(){
 
     scanf("%d",&n);//Read before fork, so that all subsequent child processes will inherit this variable
     //printf("inputed\n");
-    if(n==0){return 0;}
+    if(n==0){printf("%s","\0");return 0;}
 
-    strcpy(message.data,"");
+    char *str = NULL;
+    str = (char*)malloc((n+1)*sizeof(int));
+    if(str==NULL){perror("malloc");return 1;}
+    str[0]='\0';
+    message.mtype = 1;
+    strcpy(message.data, str);
     msgsnd(msgid,&message,1,0);
 
     pid1 = fork();
@@ -56,54 +61,25 @@ int main(){
         message.mtype = 4;
         msgsnd(msgid,&message,strlen(message.data)+1,0);
         //printf("son1 terminated\n");
- 
-    }
+        free(str);
+    }else{
+        pid2 = fork();
+        if(pid2<0){perror("fork");return 1;}
+        if(pid2==0){//son2
 
-    pid2 = fork();
-    if(pid2<0){perror("fork");return 1;}
-    if(pid2==0){//son2
+            while(1){
+                msgrcv(msgid,&message,256,2,0);
+                strcpy(str,message.data);
+                //printf("DATA2: %ld\n",strlen(message.data));
+                //printf("STR2: %ld\n",strlen(str));
+                //printf("%s\n",message.data);
 
-        while(1){
-            msgrcv(msgid,&message,256,2,0);
-            strcpy(str,message.data);
-            //printf("DATA2: %ld\n",strlen(message.data));
-            //printf("STR2: %ld\n",strlen(str));
-            //printf("%s\n",message.data);
-
-            message.data[strlen(str)]='0'+(strlen(str)/3%10);
-            message.data[strlen(str)+1]='\0';
-            //printf("%s\n",message.data);
-            //sleep(1);
-            message.mtype = 3;
-            msgsnd(msgid,&message,strlen(message.data)+1,0);
-
-            if(strlen(message.data)>=n){
-                break;
-            }
-        }
-        message.mtype = 4;
-        msgsnd(msgid,&message,strlen(message.data)+1,0);
-        //printf("son2 terminated\n");
-
-    }
-
-    pid3 = fork();
-    if(pid3<0){perror("fork");return 1;}
-    if(pid3==0){//son3
-
-        while(1){
-            msgrcv(msgid,&message,256,3,0);
-            strcpy(str,message.data);
-            //printf("%s\n",message.data);
-
-            //printf("DATA3: %ld\n",strlen(message.data));
-            //printf("STR3: %ld\n",strlen(str));
-            message.data[strlen(str)]='A'+(strlen(str)/3%26);
-            message.data[strlen(str)+1]='\0';
-            //printf("%s\n",message.data);
-            //sleep(1);
-            message.mtype = 1;
-            msgsnd(msgid,&message,strlen(message.data)+1,0);
+                message.data[strlen(str)]='0'+(strlen(str)/3%10);
+                message.data[strlen(str)+1]='\0';
+                //printf("%s\n",message.data);
+                //sleep(1);
+                message.mtype = 3;
+                msgsnd(msgid,&message,strlen(message.data)+1,0);
 
                 if(strlen(message.data)>=n){
                     break;
@@ -111,21 +87,52 @@ int main(){
             }
             message.mtype = 4;
             msgsnd(msgid,&message,strlen(message.data)+1,0);
-            //printf("son3 terminated\n");
+            //printf("son2 terminated\n");
+            free(str);
+        }else{
+            pid3 = fork();
+            if(pid3<0){perror("fork");return 1;}
+            if(pid3==0){//son3
+
+                while(1){
+                    msgrcv(msgid,&message,256,3,0);
+                    strcpy(str,message.data);
+                    //printf("%s\n",message.data);
+
+                    //printf("DATA3: %ld\n",strlen(message.data));
+                    //printf("STR3: %ld\n",strlen(str));
+                    message.data[strlen(str)]='A'+(strlen(str)/3%26);
+                    message.data[strlen(str)+1]='\0';
+                    //printf("%s\n",message.data);
+                    //sleep(1);
+                    message.mtype = 1;
+                    msgsnd(msgid,&message,strlen(message.data)+1,0);
+
+                    if(strlen(message.data)>=n){
+                        break;
+                    }
+                }
+                message.mtype = 4;
+                msgsnd(msgid,&message,strlen(message.data)+1,0);
+                //printf("son3 terminated\n");
+                free(str);
+            }else{//father
+            
+                //printf("DATA: %ld\n",strlen(message.data));
+                //printf("STR: %ld\n",strlen(str));
+                while(1){
+                    msgrcv(msgid,&message,256,4,0);
+                    if(strlen(message.data)>=n){break;}
+                }
+
+                printf("%s\n",message.data);
+
+                msgctl(msgid,IPC_RMID,0);
+                wait(NULL);wait(NULL);wait(NULL);
+                //printf("father terminated\n");
+                free(str);str=NULL;
+                return 0;
+            }
+        }
     }
-
-    //printf("DATA: %ld\n",strlen(message.data));
-    //printf("STR: %ld\n",strlen(str));
-    while(1){
-        msgrcv(msgid,&message,256,4,0);
-        if(strlen(message.data)>=n){break;}
-    }
-
-    printf("%s\n",message.data);
-
-    msgctl(msgid,IPC_RMID,0);
-    wait(NULL);wait(NULL);wait(NULL);
-    //printf("father terminated\n");
-    return 0;
-
 }
--- homework3/D/D25.c
+++ homework3/D/D25.c
@@ -3,6 +3,7 @@
 #include<sys/ipc.h>
 #include<sys/sem.h>
 #include<sys/shm.h>
+#include<sys/wait.h>
 #include<unistd.h>
 
 int main(){
@@ -10,10 +11,9 @@ int main(){
     pid_t pid1,pid2;
 
     struct sembuf V0 = {0,1,0};
-    struct sembuf P1 = {1,-1,0};
-    
-    struct sembuf V1 = {1,1,0};
     struct sembuf P0 = {0,-1,0};
+    struct sembuf V1 = {1,1,0};
+    struct sembuf P1 = {1,-1,0};
 
     scanf("%d",&N);
 
@@ -23,40 +23,65 @@ int main(){
     int semid = semget(key,2,IPC_CREAT | 0666);
     if(semid<0){perror("semget");return 1;}
 
-    int shmid = shmget(key,N*sizeof(int),IPC_CREAT | 0666);
+    int shmid = shmget(key,(N+1)*sizeof(long long),IPC_CREAT | 0666);
     if(shmid<0){perror("shmget");return 1;}
 
-    int *shmaddr = shmat(shmid,NULL,0);
-    if(shmaddr == (int*)-1){perror("shmat");return 1;}
+    long long *shmaddr = shmat(shmid,NULL,0);
+    if(shmaddr == (long long *)-1){perror("shmat");return 1;}
+
+    shmaddr[0] = 0;shmaddr[1]=1;shmaddr[N]=2;
+    semop(semid,&V1,1);//to son 1;
 
     pid1 = fork();
     if(pid1<0){perror("fork");return 1;}
     if(pid1==0){//son1
-        int flag = 1;
+        int i;
         do{
-        semop(semid,&P1,1);
-
-        semop(semid,&V0,1);
-        }while(flag); 
-
-    }
+            semop(semid,&P1,1);
+            if(shmaddr[N]==N){
+                shmdt(shmaddr);
+                break;
+            }
+            i = shmaddr[N];
+            shmaddr[i] = shmaddr[i-2] + shmaddr[i-1];
+            shmaddr[N]++;
+            //printf("son1: %lld \n",shmaddr[i]);
+            semop(semid,&V0,1);
+        }while(1); 
+        return 0;
+    }else{
 
     pid2 = fork();
     if(pid2<0){perror("fork");return 1;}
     if(pid2==0){//son2
+        int i;
         do{
             semop(semid,&P0,1);
-            if(*shmaddr == -1){
+            if(shmaddr[N]==N){
                 shmdt(shmaddr);
                 break;
             }
-        (*shmaddr)++;
-        semop(semid,&V1,1);
-    }while(1);
+            i = shmaddr[N];
+            shmaddr[i] = shmaddr[i-2] + shmaddr[i-1];
+            shmaddr[N]++;
+            //printf("son2: %lld \n",shmaddr[i]);
+            semop(semid,&V1,1);
+        }while(1);
+        return 0;
+    }else{
+    
+    wait(NULL);
+    semctl(semid,0,IPC_RMID,0);//delete all semophore
+    wait(NULL);
+
+    for(int i;i<N;i++){
+        printf("%lld ",shmaddr[i]);
     }
 
-    semctl(semid,0,IPC_RMID,0);//delete all semophore
     shmdt(shmaddr);//detaching from the shared memory segment
     shmctl(shmid,IPC_RMID,NULL);//delete shm
     return 0;
+    }
+    }
+
 }
