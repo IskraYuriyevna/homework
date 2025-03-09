d5deb0acccc0ae1381d6d045711e6e767702ca9d
IskraYuriyevna <1718318150@qq.com>
2025-03-10 05:51:41 +0800

    修改：     homework4/E/E9.cpp

--- homework4/E/E9.cpp
+++ homework4/E/E9.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
-#include <ostream>
+#include <ostream>//输出流库
 using namespace std;
 
 class Vector_2d{
 public:
-    Vector_2d(double x=0,double y=0.)
+    Vector_2d(double x=0,double y=0.)//构造函数
     {
         x_ = x;y_ = y;
     }
-    void print(const char* str) const;
-    Vector_2d operator+(const Vector_2d& op) const;
-    Vector_2d operator-(const Vector_2d& op) const;
+    void print(const char* str) const;//打印函数
+    Vector_2d operator+(const Vector_2d& op) const;//加法
+    Vector_2d operator-(const Vector_2d& op) const;//减法
     //{return Vector_2d(x_ - op.x_,y_ - op.y_);}
-    const Vector_2d& operator=(const Vector_2d& op);
-    const Vector_2d& operator+=(const Vector_2d& op);
-    Vector_2d operator*(double a)const;
+    const Vector_2d& operator=(const Vector_2d& op);//赋值
+    const Vector_2d& operator+=(const Vector_2d& op);//加赋值
+    Vector_2d operator*(double a)const;//点乘/标量乘
 
-    double& operator[](int index);
-    const Vector_2d& operator++();//++u
-    Vector_2d operator++(int);//u++
-    friend Vector_2d operator*(double a,const Vector_2d& op);
-    friend ostream& operator<<(ostream& os, const Vector_2d& op);
-    friend istream& operator>>(istream& is, Vector_2d& op);
+    double& operator[](int index);//下标操作符
+    const Vector_2d& operator++();//++u前置自增
+    Vector_2d operator++(int);//u++后置自增
+    friend Vector_2d operator*(double a,const Vector_2d& op);//友元的标量乘
+    friend ostream& operator<<(ostream& os, const Vector_2d& op);//输出流重载
+    friend istream& operator>>(istream& is, Vector_2d& op);//输入流重载
 private:
     double x_,y_;
 };
