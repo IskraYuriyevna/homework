3d00444c2005954e3ce4ed2746e0d0d3fc6047ff
IskraYuriyevna <1718318150@qq.com>
2025-03-30 21:53:02 +0800

    修改：     homework4/E/sb2.cpp

--- homework4/E/sb2.cpp
+++ homework4/E/sb2.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 
-struct Pers{
+struct Pers
+{
     char name[20];
     int num_int;
     double num_double;
@@ -14,12 +15,12 @@ void Swap(int a , int b)
     b = tmp;
 }
 
-void Swap(int *a, int *b)
-{
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
-}
+// void Swap(int *a, int *b)
+// {
+//     int tmp = *a;
+//     *a = *b;
+//     *b = tmp;
+// }
 
 template <typename T>
 void Swap(T& a, T& b)
@@ -27,34 +28,23 @@ void Swap(T& a, T& b)
     T tmp = a;
     a = b;
     b = tmp;
-    cout << "Type T << endl";
+    cout << "Type T" << endl;
 };
 
 template<>
 void Swap(Pers& a,Pers& b)
 {
-    int tmp1 = a.num_int;
+    int tmpi = a.num_int;
     double tmpd = a.num_double;
     a.num_int = b.num_int;
     a.num_double = b.num_double;
-    b.num_int = tmp1;
+    b.num_int = tmpi;
     b.num_double = tmpd;
 }
 
 template<typename T>
 void Print(T *arr ,int n)
-
-void Swap(Pers& a,Pers b){
-    int tmpi = a.num_int;
-    double tmpd = a.num_double;
-    a.num_int = b.num_int;
-    a.num_double = b.num_double;
-    b.num_int;
-
-}
-
-template<typename T>
-void Print(T *arr,int n){
+{
     for(int i = 0;i<n;i++)
         cout << arr[i] << " ";
     cout << endl;
@@ -65,45 +55,8 @@ void Swap(Type* arr1,Type* arr2, int n)
 {
     for(int i = 0;i<n;i++){
         Type tmp = arr1[i];
-        arr1[i] = 
-        arr2[i] = 
+        arr1[i] = arr2[i];
+        arr2[i] = tmp;
     }
-    cout << "Type" ;
-}
-
-
-int main(){
-    int i = 45 , j = 98;
-    cout << "i = " << i << " j = " << j << endl;
-    //Swap(i,j);
-    Swap<int>(i,j);
-    cout << "i = " << i << " j = " << j << endl;
-    double x = 3.14,y = 2.7;
-    cout << "x = " << x << " y = " << y << endl;
-    Swap(x,y);
-    cout << " x = " << x << " y = " << y << endl;
-    double *arr1,*arr2;
-    arr1 = new double[5];
-    arr2 = new double[5];
-
-    for(int i = 0;i<5;i++){
-        arr1[i] = i+i;
-        arr2[i] = 10-i;
-    }
-    Print(arr1, 5);
-    Swap(arr1,arr2);
-    Print(arr1,5);Print(arr2,5);
-    int arr[];
-    int arr[];
-
-    Pers p1 = {"A",23,3.14};
-    Pers p2 = {"B",56,2.7};
-
-    cout<<p1.name<< " "<<p1.num_int << " "<<p1.num_double <<endl;
-    cout<<p1.name<< " "<<p1.num_int << " "<<p1.num_double <<endl;
-    Swap(p1,p2);
-    cout<<p1.name<< " "<<p1.num_int << " "<<p1.num_double <<endl;
-    cout<<p2.name<< " "<<p2.num_int << " "<<p2.num_double <<endl;
-
-    return 0;
+    cout << "Type* " <<endl;
 }
