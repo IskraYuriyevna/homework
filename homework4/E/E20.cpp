e224bad664c5da097609f9e5021c60cfd3b49560
IskraYuriyevna <1718318150@qq.com>
2025-05-24 00:09:11 +0800

    修改：     homework4/E/E17.cpp 	修改：     homework4/E/E20.cpp 	修改：     homework4/E/E22.cpp 	修改：     homework4/E/E7.cpp 	新文件：   homework4/E/output/E17 	新文件：   homework4/E/output/E20 	新文件：   homework4/E/output/E20a 	修改：     homework4/E/output/E7

--- homework4/E/E20.cpp
+++ homework4/E/E20.cpp
@@ -2,25 +2,175 @@
 using namespace std;
 
 class Figure{
-
+    protected:
+        int x1_,y1_,x2_,y2_;
+    public:
+    Figure(int x1,int y1,int x2,int y2):x1_(x1),y1_(y1),x2_(x2),y2_(y2){}
+    virtual ~Figure(){}
+    virtual bool move () const = 0;
+    bool bound()const;
 };
+
+bool Figure::bound()const{
+    if(x2_>8 || y2_>8 || x2_<=0 || y2_<=0){
+        return false;
+    }else if(x1_==x2_ && y1_==y2_) {
+        return false;
+    }else{
+        return true;
+    }
+}
+
 
 class K : public Figure{ //King
-
+    public:
+    K(int x1,int y1,int x2,int y2):Figure(x1,y1,x2,y2){}
+    bool move() const;
 };
+
+bool K::move() const {
+    if(!bound()){return false;}
+    if(abs(x1_-x2_)==1 && y1_==y2_) 
+    {
+        return true;
+    }else if(abs(y1_-y2_)==1 && x1_==x2_) {
+        return true;
+    }else if(abs(x1_-x2_)==1 && abs(y1_-y2_)==1) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
 
 class Q : public Figure{ //Queen
-
+    public:
+    Q(int x1,int y1,int x2,int y2):Figure(x1,y1,x2,y2){}
+    bool move() const;
 };
+
+bool Q::move() const {
+    if(!bound()){return false;}
+    if(abs(x1_-x2_)>=1 && (y1_==y2_)) 
+    {
+        return true;
+    }else if(abs(y1_-y2_)>=1 && (x1_==x2_)) {
+        return true;
+    }else if(abs(x1_-x2_) == abs(y1_-y2_)) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
 
 class N : public Figure{ //Knight
-
+    public:
+    N(int x1,int y1,int x2,int y2):Figure(x1,y1,x2,y2){}
+    bool move() const;
 };
+
+bool N::move() const {
+    if(!bound()){return false;}
+    if(abs(y1_-y2_)==2 && (x1_==x2_)==1) {
+        return true;
+    }else if(abs(x1_-x2_)==2 && abs(y1_-y2_)==1) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
 
 class B : public Figure{ //Bishop
-
+    public:
+    B(int x1,int y1,int x2,int y2):Figure(x1,y1,x2,y2){}
+    bool move() const;
 };
 
-class R : public Figure{ //Rook
+bool B::move() const {
+    if(!bound()){return false;}
+    if(abs(x1_-x2_) == abs(y1_-y2_)) {
+        return true;
+    }else{
+        return false;
+    }
+}
 
+
+class R : public Figure{ //Rook
+    public:
+    R(int x1,int y1,int x2,int y2):Figure(x1,y1,x2,y2){}
+    bool move() const;
+};
+
+bool R::move() const {
+    if(!bound()){return false;}
+    if(abs(y1_-y2_)>=1 && x1_==x2_) {
+        return true;
+    }else if(abs(x1_-x2_)>=1 && y1_==y2_) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+class Parser{
+    private:
+        char fig_;
+        int x1,y1,x2,y2;
+    public:
+        Parser(const char * str):fig_(str[0]),x1(str[2]-'a'+1),y1(str[3]-'0'),x2(str[5]-'a'+1),y2(str[6]-'0'){}
+        char getFig() const {return fig_;}
+        int getX1() const {return x1;}
+        int getY1() const {return y1;}
+        int getX2() const {return x2;}
+        int getY2() const {return y2;}
+};
+
+
+class ChessChecker{
+    private:
+        Figure* figures;
+        char * str;
+    public:
+        ChessChecker(){
+            Parser p(gs());
+            
+            switch (p.getFig()) {
+                case ('K'):
+                    figures = new K(p.getX1(),p.getY1(),p.getX2(),p.getY2());
+                    break;
+                case ('Q'):
+                    figures = new Q(p.getX1(),p.getY1(),p.getX2(),p.getY2());
+                    break;
+                case ('N'):
+                    figures = new N(p.getX1(),p.getY1(),p.getX2(),p.getY2());
+                    break;
+                case ('B'):
+                    figures = new B(p.getX1(),p.getY1(),p.getX2(),p.getY2());
+                    break;
+                case ('R'):
+                    figures = new R(p.getX1(),p.getY1(),p.getX2(),p.getY2());
+                    break;
+                default:
+                    break;
+            }  
+        };
+        ~ChessChecker(){delete [] str;delete figures;}
+        char* gs(){
+            str = new char[8];cin.getline(str,8);return str;
+        };
+        bool check() const{if(figures->move()){return true;}else{return false;}}
+};
+
+int main(){
+    ChessChecker checker;
+    if(checker.check()){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+    return 0;
 }
--- homework4/E/E7.cpp
+++ homework4/E/E7.cpp
@@ -79,22 +79,22 @@ void Vec::print() const {
     cout << ")"<<endl;
 }
 
-// int main(void)
-// {
-//     double y1[3] = {1,2,3};
-//     double y2[5] = {2,1,0,5,7};
-//     Vec x(3, y1), y(5, y2), z(4);
-//     Vec u = x;
+int main(void)
+{
+    double y1[3] = {1,2,3};
+    double y2[5] = {2,1,0,5,7};
+    Vec x(3, y1), y(5, y2), z(4);
+    Vec u = x;
 
-//     y.print();
-//     x.print();
-//     z.print();
-//     u.print();
+    y.print();
+    x.print();
+    z.print();
+    u.print();
 
-//     x.set(23, 2);  u.set(34, 1);  z.set(-3, 3);
-//     cout << "x[2] = " << x.get(2) << endl;
-//     cout << "u[1] = " << u.get(1) << endl;
-//     cout << "euc_norma y: " << y.euc_norm() << endl;
-//     cout << "max_norma z: " << z.max_norm() << endl;
-//     return 0;
-// }
+    x.set(23, 2);  u.set(34, 1);  z.set(-3, 3);
+    cout << "x[2] = " << x.get(2) << endl;
+    cout << "u[1] = " << u.get(1) << endl;
+    cout << "euc_norma y: " << y.euc_norm() << endl;
+    cout << "max_norma z: " << z.max_norm() << endl;
+    return 0;
+}
