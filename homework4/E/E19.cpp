480d5aece2ac0abb8a58d263af8f39e4f8d9cc0d
IskraYuriyevna <1718318150@qq.com>
2025-05-23 15:08:08 +0800

    修改：     homework4/E/E19.cpp 	新文件：   homework4/E/output/E19

--- homework4/E/E19.cpp
+++ homework4/E/E19.cpp
@@ -3,17 +3,61 @@
 using namespace std;
 
 class Base{
-
+    private:
+        double x_,y_,z_; // center
+    public:
+        Base(double x,double y,double z):x_(x),y_(y),z_(z){}
+        Base(const Base& src):x_(src.x_),y_(src.y_),z_(src.z_){}
+        ~Base(){}
+        void move(double x,double y,double z){x_=x;y_=y;z_=z;}
+        virtual void print()const{ cout<< "(" << x_ << "," << y_ << "," << z_ << ")";}
+        virtual double volume()const=0;
+        virtual double area()const=0;
 };
 
-class cube : public Base{
 
+class cube : public Base{
+    private:
+        double l_; //side length
+    public:
+        cube(double x,double y,double z,double l):Base(x,y,z),l_(l){}
+        cube(const cube& src):Base(src),l_(src.l_){}
+        ~cube(){}
+        void print() const{
+            Base::print();
+            cout << " l=" << l_ << endl;
+        };
+        double volume() const{return l_*l_*l_;}
+        double area()const{return l_*l_*6;}
 };
 
 class ball : public Base{
-
+    private:
+        double r_; // radius
+    public:
+        ball(double x,double y,double z,double r):Base(x,y,z),r_(r){}
+        ball(const ball& src):Base(src),r_(src.r_){}
+        ~ball(){}
+        void print() const{
+            Base::print();
+            cout << " r=" << r_ << endl;
+        };
+        double volume() const{return 4*M_PI*r_*r_*r_/3;}
+        double area()const{return 4*M_PI*r_*r_;}
 };
 
 class cylinder : public Base{
-
+    private:
+        double r_,h_; // radius and height
+    public:
+        cylinder(double x,double y,double z,double r,double h):Base(x,y,z),r_(r),h_(h){}
+        cylinder(const cylinder& src):Base(src),r_(src.r_),h_(src.h_){}
+        ~cylinder(){}
+        void print() const{
+            Base::print();
+            cout << " r=" << r_ << " h=" << h_ <<endl;
+        };
+        double volume() const{return M_PI*r_*r_*h_;}
+        double area()const{return M_PI*r_*r_*2 + 2*M_PI*r_*h_;}
 };
+
