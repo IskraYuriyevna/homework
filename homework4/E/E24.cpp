28a5b36c052cdef0cae52517452c8efb2414b657
IskraYuriyevna <1718318150@qq.com>
2025-05-24 20:55:58 +0800

    修改：     homework4/E/E24.cpp 	新文件：   homework4/nE9.cpp 	新文件：   homework4/test.cpp 	新文件：   homework4/test2.cpp

--- homework4/E/E24.cpp
+++ homework4/E/E24.cpp
@@ -1,1 +1,43 @@
 #include<iostream>
+#include<string>
+using namespace std;
+
+class Exception{
+    private:
+        string comment_;
+    public:
+        Exception(const char * comment): comment_(comment){}
+        Exception(const Exception & other): comment_(other.comment_){}
+        ~Exception(){}
+        const string GetComment() const { return comment_; }
+    };
+
+class WrongNumber : public Exception{
+    private:
+        int errno_;
+    public:
+        WrongNumber(const char* comment):comment_(comment){}
+
+};
+
+class Player{
+    private:
+        string team_;
+        int number_;
+        static int players_[12];
+    public:
+        Player();
+        ~Player();
+        void Print();
+        void Print_teams();
+};
+
+
+int main()
+{
+    try {
+
+    }catch () {
+        
+    }
+}
