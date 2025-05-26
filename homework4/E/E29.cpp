f510bb5817defec5ad1b238fedd05da90d0febc0
IskraYuriyevna <1718318150@qq.com>
2025-05-26 15:21:43 +0800

    修改：     homework4/E/E29.cpp 	修改：     homework4/E/output/E29

--- homework4/E/E29.cpp
+++ homework4/E/E29.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
-#include<vector>
+
 using namespace std;
 
 template<class T>
 class Set{
     private:
-        vector<T> set;
+        T* set_;
+        int size_;
+        void ins_sort();
+
     public:
-        Set(){}
+        Set(){set_ = nullptr;size_=0;}
         Set(T* arr,int size);
-        ~Set(){}
+        Set(const Set& other);
+        ~Set(){delete[] set_;};
         void add(const T& i);
         void add(const T* arr,int size);
         void del(const T& i);
@@ -17,35 +21,54 @@ class Set{
         void print();
         Set operator*(const Set& op)const;
         Set operator+(const Set& op)const;
-        void ins_sort();
+        Set<T>& operator=(const Set& other);
+        
 }; 
 
 template<class T>
-void Set<T>::ins_sort(){
-    int n = set.size();
-    for(int i = 1;i<n;i++){
-        T key = set[i];
-        int j  = i - 1;
-        while(j>=0 && set[j]>key)
-        {
-            set[j+1]=set[j];
-            j--;
-        }
-        set[j+1] = key;
+Set<T>::Set(const Set& other):set_(new T[other.size_]),size_(other.size_){
+    for (int i = 0;i<size_;i++) {
+        set_[i] = other.set_[i];
     }
 }
 
 template<class T>
 Set<T>::Set(T* arr,int size){
+    set_ = new T[size];
     for(int i=0;i<size;i++){
-        add(arr[i]);
+        set_[i] = arr[i];
+    }
+    size_ = size;
+}
+
+template<class T>
+void Set<T>::ins_sort(){
+    int n = size_;
+    for(int i = 1;i<n;i++){
+        T key = set_[i];
+        int j  = i - 1;
+        while(j>=0 && set_[j]>key)
+        {
+            set_[j+1]=set_[j];
+            j--;
+        }
+        set_[j+1] = key;
     }
 }
+
 
 template<class T>
 void Set<T>::add(const T& i){
     if(!in(i)){
-        set.push_back(i);
+        T* tmp = new T[size_+1];
+        for(int j=0;j<size_;j++){
+            tmp[j]=set_[j];
+        }
+        tmp[size_] = i;
+
+        delete[] set_;
+        set_ = tmp;
+        size_ ++;
     }
 }
 
@@ -56,19 +79,37 @@ void Set<T>::add(const T* arr,int size){
     }
 }
 
+
+//
 template<class T>
-void Set<T>::del(const T& i){
-    for(int j=0;j<set.size();j++){
-        if(set[j]==i){
-            set.erase(set.begin()+j);
+void Set<T>::del(const T& i) {
+    if(in(i)){
+        int pos=-1;
+        for (int j = 0; j < size_; ++j) {
+            if (set_[j] == i) {
+                pos = j;
+                break;
+            }
         }
+
+        T* tmp = new T[size_ - 1];
+        for (int j = 0; j < pos; ++j) {
+            tmp[j] = set_[j];
+        }
+        for (int j = pos; j < size_ - 1; ++j) {
+            tmp[j] = set_[j + 1];
+        }
+
+        delete[] set_;
+        set_ = tmp;
+        size_--;
     }
 }
 
 template<class T>
 bool Set<T>::in(const T& i)const{
-    for(int j=0;j<set.size();j++){
-        if(set[j]==i){
+    for(int j=0;j<size_;j++){
+        if(set_[j]==i){
             return true;
         }
     }
@@ -78,8 +119,8 @@ bool Set<T>::in(const T& i)const{
 template<class T>
 void Set<T>::print(){
     ins_sort();
-    for(int j=0;j<set.size();j++){
-        cout << set[j] << " ";
+    for(int j=0;j<size_;j++){
+        cout << set_[j] << " ";
     }
     cout << endl;
 }
@@ -87,10 +128,10 @@ void Set<T>::print(){
 template<class T>
 Set<T> Set<T>::operator*(const Set& op)const{
     Set res;
-    for(int j=0;j<set.size();j++){
-        for(int k=0;k<op.set.size();k++){
-            if(set[j]==op.set[k]){
-                res.add(set[j]);
+    for(int j=0;j<size_;j++){
+        for(int k=0;k<op.size_;k++){
+            if(set_[j]==op.set_[k]){
+                res.add(set_[j]);
                 break;
             }
         }
@@ -101,11 +142,25 @@ Set<T> Set<T>::operator*(const Set& op)const{
 template<class T>
 Set<T> Set<T>::operator+(const Set& op)const{
     Set res;
-    for(int j=0;j<set.size();j++){
-        res.add(set[j]);
+    for(int j=0;j<size_;j++){
+        res.add(set_[j]);
     }
-    for(int j=0;j<op.set.size();j++){
-        res.add(op.set[j]);
+    for(int j=0;j<op.size_;j++){
+        res.add(op.set_[j]);
     }
     return res;
 }
+
+template<class T>
+Set<T>& Set<T>::operator=(const Set& other) {
+    if (this != &other) {
+        delete[] set_;
+        size_ = other.size_;
+        set_ = new T[size_];
+        for (int i = 0; i < size_; ++i) {
+            set_[i] = other.set_[i];
+        }
+    }
+    return *this;
+}
+
